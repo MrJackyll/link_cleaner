f076ac57c34b2adec31037f5aeda3a2ff06ad429
MrJackyll <49453589+MrJackyll@users.noreply.github.com>
2025-04-20 17:16:21 +0300

    Add files via upload

---
+++ link_cleaner.py
@@ -0,0 +1,10 @@
+import re
+
+def trim_url(url):
+    trimmed_url = re.sub(r'\?.*$', '', url)
+    return trimmed_url
+
+if __name__ == "__main__":
+    user_input = input("Enter your link: ")
+    result = trim_url(user_input)
+    print("Clear link:", result)
